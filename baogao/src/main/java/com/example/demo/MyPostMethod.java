c044ab78e53e21a7e7f30bd157d5d5beb2252726
huwenhua95 <1751400385@qq.com>
2019-08-26 21:54:14 +0800

    tijiao

--- baogao/src/main/java/com/example/demo/MyPostMethod.java
+++ baogao/src/main/java/com/example/demo/MyPostMethod.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -29,6 +28,27 @@ public class MyPostMethod {
         return "登陆成功";
     }
         return "用户名或密码错误";
+    }
+
+    @RequestMapping(value = "/getUserList", method = RequestMethod.POST)
+    @ApiOperation(value = "获取用户列表", httpMethod = "POST")
+    public String getUserList(HttpServletRequest request,
+                              @RequestBody User u) {
+    User user;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie c:cookies){
+           if(c.getName()=="login"
+              && c.getValue()=="true"
+                   &&u.getUserName()=="zhangsan"
+                   &&u.getPassword()=="123456") {
+               user=new User();
+               user.setName("lisi");
+               user.setAge("18");
+               user.setSex("man");
+               return user.toString();
+           }
+        }
+        return "参数不合法";
     }
 }
 
